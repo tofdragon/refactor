bd1360b539f99ef7763f41ff91a17456d2b6985c
孙晶 <sunjing@sinobest.cn>
2020-01-05 13:36:03 +0800

    refactor(code): 将大函数分拆为小函数

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -10,51 +10,61 @@ final class GildedRose {
 
     public void updateQuality() {
         for (Item item : getItems()) {
-            if (!item.isAged() && !item.isBackStage()) {
-                if (item.quality > 0) {
-                    if (!item.isSulfuras()) {
-                        item.quality = item.quality - 1;
+            updateCommonQuality(item);
+            updateSellWhenNotSulfuras(item);
+            updateWhenSellLessThanZero(item);
+        }
+    }
+
+    private void updateCommonQuality(Item item) {
+        if (!item.isAged() && !item.isBackStage()) {
+            if (item.quality > 0) {
+                if (!item.isSulfuras()) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.isBackStage()) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void updateSellWhenNotSulfuras(Item item) {
+        if (!item.isSulfuras()) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void updateWhenSellLessThanZero(Item item) {
+        if (item.sellIn < 0) {
+            if (!item.isAged()) {
+                if (!item.isBackStage()) {
+                    if (item.quality > 0) {
+                        if (!item.isSulfuras()) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.isBackStage()) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.isSulfuras()) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.isAged()) {
-                    if (!item.isBackStage()) {
-                        if (item.quality > 0) {
-                            if (!item.isSulfuras()) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
                 }
             }
         }
