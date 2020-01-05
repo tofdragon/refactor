89dca54fcafc09efed6544a3ecf5630b7204171f
孙晶 <sunjing@sinobest.cn>
2020-01-05 13:38:26 +0800

    refactor(code): 根据迪米特法则,移动方法到Item中

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/Item.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,66 @@ public class Item {
         this.quality = quality;
     }
 
+    public void updateQuality() {
+        updateCommonQuality();
+        updateSellWhenNotSulfuras();
+        updateWhenSellLessThanZero();
+    }
+
+    private void updateCommonQuality() {
+        if (!isAged() && !isBackStage()) {
+            if (quality > 0) {
+                if (!isSulfuras()) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (isBackStage()) {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void updateSellWhenNotSulfuras() {
+        if (!isSulfuras()) {
+            sellIn = sellIn - 1;
+        }
+    }
+
+    private void updateWhenSellLessThanZero() {
+        if (sellIn < 0) {
+            if (!isAged()) {
+                if (!isBackStage()) {
+                    if (quality > 0) {
+                        if (!isSulfuras()) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
+
     boolean isBackStage() {
         return name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
