8a2a24fdc4134323e6297d7f23e2fb801191f676
孙晶 <sunjing@sinobest.cn>
2020-01-05 11:48:47 +0800

    refactor(code): 所有魔法值字符串替换为方法

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -7,16 +7,11 @@ class GildedRose {
         this.items = items;
     }
 
-    private boolean isBackStage(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !isBackStage(items[i])) {
+            if (!isAged(items[i]) && !isBackStage(items[i])) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!isSulfuras(items[i])) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -40,15 +35,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!isSulfuras(items[i])) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
+                if (!isAged(items[i])) {
                     if (!isBackStage(items[i])) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!isSulfuras(items[i])) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
@@ -62,5 +57,17 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isBackStage(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isAged(Item item) {
+        return item.name.equals("Aged Brie");
     }
 }
