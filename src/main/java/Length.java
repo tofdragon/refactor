b34f75c0d8fc861df44f9984478e1500df382452
孙晶 <sunjing@sinobest.cn>
2020-02-11 17:13:40 +0800

    refactor(code): factory method

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,6 +1,4 @@
 public class Length {
-    private final double value;
-    private final String unit;
 
     public static final String FOOT = "f";
 
@@ -8,9 +6,25 @@ public class Length {
 
     public static final String INCH = "inch";
 
-    public Length(double val, String unit) {
-        this.value = val;
+    private final double value;
+
+    private final String unit;
+
+    private Length(double value, String unit) {
+        this.value = value;
         this.unit = unit;
+    }
+
+    public static Length createFoot(double value) {
+        return new Length(value, FOOT);
+    }
+
+    public static Length createYard(double value) {
+        return new Length(value, YARD);
+    }
+
+    public static Length createInch(double value) {
+        return new Length(value, INCH);
     }
 
     public Length as(String target) {
@@ -31,11 +45,11 @@ public class Length {
 
     private Length footTo(String target) {
         if (target.equals(YARD)) {
-            return new Length(this.value / 3, target);
+            return Length.createYard(this.value / 3);
         }
 
         if (target.equals(INCH)) {
-            return new Length(this.value * 12, target);
+            return Length.createInch(this.value * 12);
         }
 
         return this;
@@ -43,11 +57,11 @@ public class Length {
 
     private Length yardTo(String target) {
         if (target.equals(INCH)) {
-            return new Length(this.value * 36, target);
+            return Length.createInch(this.value * 36);
         }
 
         if (target.equals(FOOT)) {
-            return new Length(this.value * 3, target);
+            return Length.createFoot(this.value * 3);
         }
 
         return this;
@@ -55,11 +69,11 @@ public class Length {
 
     private Length inchTo(String target) {
         if (target.equals(FOOT)) {
-            return new Length(this.value / 12, target);
+            return Length.createFoot(this.value / 12);
         }
 
         if (target.equals(YARD)) {
-            return new Length(this.value / 36, target);
+            return Length.createYard(this.value / 36);
         }
 
         return this;
