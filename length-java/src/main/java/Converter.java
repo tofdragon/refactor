9cef34978f368b37f37beb8522d182ae8c000ad9
孙晶 <sunjing@sinobest.cn>
2020-06-21 16:55:24 +0800

    refactor(code): static变量命名规范

--- length-java/src/main/java/Converter.java
+++ length-java/src/main/java/Converter.java
@@ -8,20 +8,20 @@ import java.util.List;
  */
 public class Converter {
 
-    private static List<Pair> pairs = new ArrayList<>();
+    private static final List<Pair> PAIRS = new ArrayList<>();
 
     static {
-        pairs.add(Pair.create(Unit.FOOT, Unit.YARD, 1/3d));
-        pairs.add(Pair.create(Unit.FOOT, Unit.INCH, 12d));
-        pairs.add(Pair.create(Unit.FOOT, Unit.FOOT, 1d));
+        PAIRS.add(Pair.create(Unit.FOOT, Unit.YARD, 1/3d));
+        PAIRS.add(Pair.create(Unit.FOOT, Unit.INCH, 12d));
+        PAIRS.add(Pair.create(Unit.FOOT, Unit.FOOT, 1d));
 
-        pairs.add(Pair.create(Unit.YARD, Unit.INCH, 36d));
-        pairs.add(Pair.create(Unit.YARD, Unit.FOOT, 3d));
-        pairs.add(Pair.create(Unit.YARD, Unit.YARD, 1d));
+        PAIRS.add(Pair.create(Unit.YARD, Unit.INCH, 36d));
+        PAIRS.add(Pair.create(Unit.YARD, Unit.FOOT, 3d));
+        PAIRS.add(Pair.create(Unit.YARD, Unit.YARD, 1d));
 
-        pairs.add(Pair.create(Unit.INCH, Unit.FOOT, 1/12d));
-        pairs.add(Pair.create(Unit.INCH, Unit.YARD, 1/36d));
-        pairs.add(Pair.create(Unit.INCH, Unit.INCH, 1d));
+        PAIRS.add(Pair.create(Unit.INCH, Unit.FOOT, 1/12d));
+        PAIRS.add(Pair.create(Unit.INCH, Unit.YARD, 1/36d));
+        PAIRS.add(Pair.create(Unit.INCH, Unit.INCH, 1d));
     }
 
     public static double to(Unit from, Unit to, double value) {
@@ -30,7 +30,7 @@ public class Converter {
     }
 
     private static Pair findBy(Unit from, Unit to) {
-        for (Pair pair : pairs) {
+        for (Pair pair : PAIRS) {
             if (pair.equalsFormAndTo(from, to)) {
                 return pair;
             }
