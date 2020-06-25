2ec27b80d4745935d4e4acacd0f829923169a0da
孙晶 <sunjing@sinobest.cn>
2020-06-25 16:03:15 +0800

    refactor(code): 卫语句

--- gildedrose-java/src/main/java/com/gildedrose/Item.java
+++ gildedrose-java/src/main/java/com/gildedrose/Item.java
@@ -3,9 +3,11 @@ package com.gildedrose;
 public class Item {
 
     private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";;
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";;
-    
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    ;
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    ;
+
     public String name;
 
     public int sellIn;
@@ -61,23 +63,31 @@ public class Item {
     }
 
     private void expired() {
-        if (sellIn < 0) {
-            if (!name.equals(AGED_BRIE)) {
-                if (!name.equals(BACKSTAGE)) {
-                    if (quality > 0) {
-                        if (!name.equals(SULFURAS)) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    quality = quality - quality;
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
+        if (sellIn >= 0) {
+            return;
         }
+
+        if (name.equals(AGED_BRIE)) {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+            return;
+        }
+
+        if (name.equals(BACKSTAGE)) {
+            quality = 0;
+            return;
+        }
+
+        if (quality <= 0) {
+            return;
+        }
+
+        if (name.equals(SULFURAS)) {
+            return;
+        }
+
+        quality = quality - 1;
     }
 
     @Override
