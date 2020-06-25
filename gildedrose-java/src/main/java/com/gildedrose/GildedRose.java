b1624ff2495f8d23cda6e6e8cdb2792d8c6e853b
孙晶 <sunjing@sinobest.cn>
2020-06-25 15:35:37 +0800

    refactor(code): extract method

--- gildedrose-java/src/main/java/com/gildedrose/GildedRose.java
+++ gildedrose-java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,51 +13,60 @@ class GildedRose {
             String backstage = "Backstage passes to a TAFKAL80ETC concert";
             String sulfuras = "Sulfuras, Hand of Ragnaros";
 
-            if (!item.name.equals(agedBrie)
-                    && !item.name.equals(backstage)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(sulfuras)) {
-                        item.quality = item.quality - 1;
+            updateQuality(item, agedBrie, backstage, sulfuras);
+            updateSellIn(item, sulfuras);
+            sellInLessZero(item, agedBrie, backstage, sulfuras);
+        }
+    }
+
+    private void sellInLessZero(Item item, String agedBrie, String backstage, String sulfuras) {
+        if (item.sell_in < 0) {
+            if (!item.name.equals(agedBrie)) {
+                if (!item.name.equals(backstage)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(sulfuras)) {
+                            item.quality = item.quality - 1;
+                        }
                     }
+                } else {
+                    item.quality = item.quality - item.quality;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals(backstage)) {
-                        if (item.sell_in < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sell_in < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
             }
+        }
+    }
 
-            if (!item.name.equals(sulfuras)) {
-                item.sell_in = item.sell_in - 1;
+    private void updateSellIn(Item item, String sulfuras) {
+        if (!item.name.equals(sulfuras)) {
+            item.sell_in = item.sell_in - 1;
+        }
+    }
+
+    private void updateQuality(Item item, String agedBrie, String backstage, String sulfuras) {
+        if (!item.name.equals(agedBrie) && !item.name.equals(backstage)) {
+            if (item.quality > 0) {
+                if (!item.name.equals(sulfuras)) {
+                    item.quality = item.quality - 1;
+                }
             }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-            if (item.sell_in < 0) {
-                if (!item.name.equals(agedBrie)) {
-                    if (!item.name.equals(backstage)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(sulfuras)) {
-                                item.quality = item.quality - 1;
-                            }
+                if (item.name.equals(backstage)) {
+                    if (item.sell_in < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+
+                    if (item.sell_in < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
