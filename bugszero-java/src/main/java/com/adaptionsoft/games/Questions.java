4864d1f09663ff8c09b958074ba2611c75d61de6
孙晶 <sunjing@sinobest.cn>
2020-06-26 18:19:16 +0800

    refactor(code): remove repeat code

--- bugszero-java/src/main/java/com/adaptionsoft/games/Questions.java
+++ bugszero-java/src/main/java/com/adaptionsoft/games/Questions.java
@@ -1,20 +1,12 @@
 package com.adaptionsoft.games;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 /**
  * @author sunjing
  */
 public class Questions {
-
-    private final String POP_QUESTION = "Pop";
-    private final String SCIENCE_QUESTION = "Science";
-    private final String SPORTS_QUESTION = "Sports";
-    private final String ROCK_QUESTION = "Rock";
-    private final String BLUES_QUESTION = "Blues";
-    private final String HISTORY_QUESTION = "History";
 
     private Map<Category, Question> currentCategoryToQuestion = new HashMap<>();
 
@@ -31,37 +23,9 @@ public class Questions {
         removeQuestion(place);
     }
 
-    private void removeQuestion(Category currentCategory) {
-        currentCategoryToQuestion.get(currentCategory).removeQuestion();
-    }
-
     private void removeQuestion(int currentPlace) {
-        Category currentCategory = currentCategory(currentPlace);
+        Category currentCategory = Category.values()[currentPlace % Category.values().length];
         System.out.println("The category is " + currentCategory.getName());
-        removeQuestion(currentCategory);
-    }
-
-    private Category currentCategory(int currentPlace) {
-        if (currentPlace % 6 == 0) {
-            return Category.POP;
-        }
-
-        if (currentPlace % 6 == 1) {
-            return Category.SCIENCE;
-        }
-
-        if (currentPlace % 6 == 2) {
-            return Category.SPORTS;
-        }
-
-        if (currentPlace % 6 == 3) {
-            return Category.ROCK;
-        }
-
-        if (currentPlace % 6 == 4) {
-            return Category.BLUES;
-        }
-
-        return Category.HISTORY;
+        currentCategoryToQuestion.get(currentCategory).removeQuestion();
     }
 }
