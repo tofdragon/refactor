ead7bc0a1d152f42fec6d5f2966485e98003925a
孙晶 <sunjing@sinobest.cn>
2020-07-19 13:27:34 +0800

    refactor(code): 替换集合

--- calisthenics-java/src/main/java/com/theladders/avital/cc/FailedApplications.java
+++ calisthenics-java/src/main/java/com/theladders/avital/cc/FailedApplications.java
@@ -1,22 +1,20 @@
 package com.theladders.avital.cc;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author sunjing
  */
 final class FailedApplications {
 
-    private final List<JobApplication> failedApplications = new ArrayList<>();
+    private final JobApplications failedApplications = new JobApplications();
 
     void add(String jobSeekerName, Job job, String employerName, LocalDate applicationTime) {
         failedApplications.add(JobApplication.create(jobSeekerName, employerName, job, applicationTime));
     }
 
     int getUnsuccessfulApplications(String employerName, String jobName) {
-        return (int) failedApplications.stream().filter(job -> job.getJob().getJobName().equals(jobName)
+        return (int) failedApplications.getJobApplications().stream().filter(job -> job.getJob().getJobName().equals(jobName)
                 && job.getEmployerName().equals(employerName)).count();
     }
 }
