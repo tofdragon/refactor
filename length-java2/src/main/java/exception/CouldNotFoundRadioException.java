4829690ea098af981a53386aabdda229574870a4
孙晶 <sunjing@sinobest.cn>
2020-10-18 10:09:14 +0800

    refactor(code): reformat code + reformat imports

--- length-java2/src/main/java/exception/CouldNotFoundRadioException.java
+++ length-java2/src/main/java/exception/CouldNotFoundRadioException.java
@@ -5,7 +5,7 @@ import unit.Unit;
 /**
  * @author sunjing
  */
-public final class CouldNotFoundRadioException extends RuntimeException{
+public final class CouldNotFoundRadioException extends RuntimeException {
 
     private static final String MESSAGE = "%s to %s radio, could not found!";
 
--- length-java2/src/main/java/pair/Pairs.java
+++ length-java2/src/main/java/pair/Pairs.java
@@ -16,7 +16,7 @@ public final class Pairs {
 
     static {
         PAIRS.add(Pair.create(Unit.FOOT, Unit.FOOT, 1d));
-        PAIRS.add(Pair.create(Unit.FOOT, Unit.YARD, 1/3d));
+        PAIRS.add(Pair.create(Unit.FOOT, Unit.YARD, 1 / 3d));
         PAIRS.add(Pair.create(Unit.FOOT, Unit.INCH, 12d));
 
         PAIRS.add(Pair.create(Unit.YARD, Unit.YARD, 1d));
@@ -24,8 +24,8 @@ public final class Pairs {
         PAIRS.add(Pair.create(Unit.YARD, Unit.FOOT, 3d));
 
         PAIRS.add(Pair.create(Unit.INCH, Unit.INCH, 1d));
-        PAIRS.add(Pair.create(Unit.INCH, Unit.FOOT, 1/12d));
-        PAIRS.add(Pair.create(Unit.INCH, Unit.YARD, 1/36d));
+        PAIRS.add(Pair.create(Unit.INCH, Unit.FOOT, 1 / 12d));
+        PAIRS.add(Pair.create(Unit.INCH, Unit.YARD, 1 / 36d));
     }
 
     public static Double radio(Unit source, Unit target) {
