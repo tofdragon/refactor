4d17377b862ff7858e1deb4bf42d75569c93d33c
孙晶 <sunjing@sinobest.cn>
2020-10-18 10:07:39 +0800

    refactor(code): fix idea

--- length-java2/src/main/java/exception/CouldNotFoundRadioException.java
+++ length-java2/src/main/java/exception/CouldNotFoundRadioException.java
@@ -7,7 +7,7 @@ import unit.Unit;
  */
 public final class CouldNotFoundRadioException extends RuntimeException{
 
-    private static final String MESSAGE = "%s to %s radio, colud not found!";
+    private static final String MESSAGE = "%s to %s radio, could not found!";
 
     public CouldNotFoundRadioException(Unit source, Unit target) {
         super(String.format(MESSAGE, source, target));
