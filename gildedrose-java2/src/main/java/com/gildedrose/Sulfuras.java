7f65aaad8d573f5a6966abe405096c68aca4a5cd
孙晶 <sunjing@sinobest.cn>
2020-10-18 19:55:14 +0800

    refactor(code): add class

---
+++ gildedrose-java2/src/main/java/com/gildedrose/Sulfuras.java
@@ -0,0 +1,11 @@
+package com.gildedrose;
+
+/**
+ * @author sunjing
+ */
+public final class Sulfuras extends Item{
+
+    public Sulfuras(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
