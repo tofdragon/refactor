1ebc3afb389e565befc1d4b73bd87e82b50790e7
孙晶 <sunjing@sinobest.cn>
2020-10-18 20:11:15 +0800

    refactor(code): remove method parameter

--- gildedrose-java2/src/main/java/com/gildedrose/AgedBrie.java
+++ gildedrose-java2/src/main/java/com/gildedrose/AgedBrie.java
@@ -7,8 +7,8 @@ public final class AgedBrie extends Item {
 
     private static final String AGED_BRIE = "Aged Brie";
 
-    public AgedBrie(String name, int sellIn, int quality) {
-        super(name, sellIn, quality);
+    public AgedBrie(int sellIn, int quality) {
+        super(AGED_BRIE, sellIn, quality);
     }
 
     @Override
--- gildedrose-java2/src/test/java/com/gildedrose/TextTestFixture.java
+++ gildedrose-java2/src/test/java/com/gildedrose/TextTestFixture.java
@@ -15,7 +15,7 @@ public class TextTestFixture {
 
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
-                new AgedBrie("Aged Brie", 2, 0), //
+                new AgedBrie(2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Sulfuras( 0, 80), //
                 new Sulfuras( -1, 80),
