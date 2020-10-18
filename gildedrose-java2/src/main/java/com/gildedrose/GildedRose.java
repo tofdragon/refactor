ddd3fd6080043e6bb99db271c2ac7ff584359fd5
孙晶 <sunjing@sinobest.cn>
2020-10-18 18:13:14 +0800

    refactor(code): rename

--- gildedrose-java2/src/main/java/com/gildedrose/GildedRose.java
+++ gildedrose-java2/src/main/java/com/gildedrose/GildedRose.java
@@ -21,13 +21,13 @@ class GildedRose {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sell_in < 11) {
+                        if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
-                        if (item.sell_in < 6) {
+                        if (item.sellIn < 6) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
@@ -37,10 +37,10 @@ class GildedRose {
             }
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sell_in = item.sell_in - 1;
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sell_in < 0) {
+            if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0) {
