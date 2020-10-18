bc084bec6abdcee33a444253f93a8aaacb7f2c35
孙晶 <sunjing@sinobest.cn>
2020-10-18 18:06:30 +0800

    refactor(code): safe test

--- gildedrose-java2/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gildedrose-java2/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,9 +1,15 @@
 package com.gildedrose;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+import com.google.common.io.Files;
 
 public class GildedRoseTest {
 
@@ -15,6 +21,13 @@ public class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
         assertThat(app.items[0].quality, is(4));
         assertThat(app.items[0].sell_in, is(0));
+    }
+
+    @Test
+    public void should_equals_baseline_when_is_3_days() throws IOException {
+        String actual = TextTestFixture.baseLine(3);
+        String baseLine = Files.toString(new File("src/test/java/com/gildedrose/baseLine3.txt"), Charset.forName("utf-8"));
+        Assert.assertEquals(baseLine, actual);
     }
 
 }
