7928244be8ffd1f1dd343ccc4f212cd7124902fc
孙晶 <sunjing@sinobest.cn>
2020-10-18 18:21:12 +0800

    refactor(code): extract method

--- gildedrose-java2/src/main/java/com/gildedrose/GildedRose.java
+++ gildedrose-java2/src/main/java/com/gildedrose/GildedRose.java
@@ -16,26 +16,27 @@ class GildedRose {
 
     public void passOneDay() {
         for (Item item : items) {
-
             updateQuality(item);
-
             updateSellIn(item);
+            updateQualityWhenExpired(item);
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
+    private void updateQualityWhenExpired(Item item) {
+        if (item.sellIn < 0) {
+            if (!item.name.equals(AGED_BRIE)) {
+                if (!item.name.equals(BACKSTAGE)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS)) {
+                            item.quality = item.quality - 1;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
