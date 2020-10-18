322c1a3b98190864f15866e6f7d7c7ad89302331
孙晶 <sunjing@sinobest.cn>
2020-10-18 20:07:48 +0800

    refactor(code): create AgedBrie

---
+++ gildedrose-java2/src/main/java/com/gildedrose/AgedBrie.java
@@ -0,0 +1,11 @@
+package com.gildedrose;
+
+/**
+ * @author sunjing
+ */
+public final class AgedBrie extends Item {
+
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
