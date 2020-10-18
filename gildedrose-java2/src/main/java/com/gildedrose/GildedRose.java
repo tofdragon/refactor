950a3771569049b28d80e9de9c17eeb7c7b10365
孙晶 <sunjing@sinobest.cn>
2020-10-18 18:17:25 +0800

    refactor(code): extract field

--- gildedrose-java2/src/main/java/com/gildedrose/GildedRose.java
+++ gildedrose-java2/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,13 @@
 package com.gildedrose;
 
 class GildedRose {
+
+    private static final String AGED_BRIE = "Aged Brie";
+
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,14 +16,11 @@ class GildedRose {
 
     public void passOneDay() {
         for (Item item : items) {
-            final String agedBrie = "Aged Brie";
-            final String backstage = "Backstage passes to a TAFKAL80ETC concert";
-            final String sulfuras = "Sulfuras, Hand of Ragnaros";
 
-            if (!item.name.equals(agedBrie)
-                    && !item.name.equals(backstage)) {
+            if (!item.name.equals(AGED_BRIE)
+                    && !item.name.equals(BACKSTAGE)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals(sulfuras)) {
+                    if (!item.name.equals(SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -24,7 +28,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals(backstage)) {
+                    if (item.name.equals(BACKSTAGE)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -40,15 +44,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals(sulfuras)) {
+            if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(agedBrie)) {
-                    if (!item.name.equals(backstage)) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals(sulfuras)) {
+                            if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
